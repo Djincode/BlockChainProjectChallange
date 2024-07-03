085efae8957e4e00301fccec25ad18f55bdbe884
Djincode <yaqubprviz@gmail.com>
2024-07-03 22:19:27 +0400

    bundan onceki versiyaya diqqet

--- day6/contracts/PriceConverter.sol
+++ day6/contracts/PriceConverter.sol
@@ -1,25 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
- 
- 
-function getPrice(AggregatorV3Interface dataFeed) internal  view  returns (uint256) {
-    
-   (,int answer,,,) = dataFeed.latestRoundData();
-   return uint256 (answer * 1e10);
-}
+  function getPrice(AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    (, int256 answer, , , ) = priceFeed.latestRoundData();
+    // ETH/USD rate in 18 digit
+    return uint256(answer * 10000000000);
+  }
 
-function getVersion () internal  view  returns(uint256) {
-    AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-     return dataFeed.version();
-}
-
-function getConversationRate(uint256 ethAmount, AggregatorV3Interface dataFeed ) internal  view returns(uint256){
-uint256 ethPrice = getPrice(dataFeed);
-uint256 AmountInUsd = (ethPrice * ethAmount)/1e18;
-return AmountInUsd;
-}
-
+  // 1000000000
+  // call it get fiatConversionRate, since it assumes something about decimals
+  // It wouldn't work for every aggregator
+  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed)
+    internal
+    view
+    returns (uint256)
+  {
+    uint256 ethPrice = getPrice(priceFeed);
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+    // the actual ETH/USD conversation rate, after adjusting the extra 0s.
+    return ethAmountInUsd;
+  }
 }
--- day6/hardhat.config.js
+++ day6/hardhat.config.js
@@ -4,18 +4,20 @@ require("dotenv").config();
 require("hardhat-gas-reporter");
 require("@nomicfoundation/hardhat-verify");
 require("@nomicfoundation/hardhat-ethers");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-ethers")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 
-const sepolia_url = process.env.SEPOLIA_RPC_URL;
-const sepolia_private_key = process.env.SEPOLIA_PRIVATE_KEY;
-const ETH_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 module.exports = {
   solidity: "0.8.24",
   etherscan: {
-    apiKey: ETH_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
   },
   gasReporter: {
     enabled: true,
@@ -30,9 +32,10 @@ module.exports = {
       chainId: 31337,
     },
     sepolia: {
-        url: sepolia_url,
-        accounts: [sepolia_private_key],
+        url: SEPOLIA_RPC_URL,
+        accounts: [PRIVATE_KEY],
         chainId: 11155111,
+        blockConfirmations: 6,
     },
   },
   namedAccounts: {
@@ -40,5 +43,8 @@ module.exports = {
         default: 0, // here this will by default take the first account as deployer
         1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
+},
+mocha: {
+  timeout: 500000,
 },
 };
